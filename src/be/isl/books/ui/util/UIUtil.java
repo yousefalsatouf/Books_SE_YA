66d83bd46474b47bbecccb796c4fbd4eec042670
yousefalsatouf <yousef@airobot.eu>
2023-10-22 12:19:44 +0200

    adding ui util helpers

---
+++ src/be/isl/books/ui/util/UIUtil.java
@@ -0,0 +1,15 @@
+package be.isl.books.ui.util;
+
+import java.util.Scanner;
+
+public class UIUtil {
+    public static int getIntInput(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        return scanner.nextInt();
+    }
+
+    public static String getStringInput(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+}
